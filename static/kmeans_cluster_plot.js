195f7b5fada16b68029b3b2157bce282be4375d9
Unknown <sraja2911@gmail.com>
2019-06-18 13:02:02 -0400

    Updated
    Updated

---
+++ static/kmeans_cluster_plot.js
@@ -0,0 +1,8 @@
+define(function(){
+
+		var kmeans_cluster_plot = {
+				kmeans_cluster_plot: function(slideid,imgpath,name){
+				}//end of named/internal function
+		} //end of var
+	return kmeans_cluster_plot;
+});//end of define
